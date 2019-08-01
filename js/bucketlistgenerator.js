dd71a77d26535b1ca6c291d33cced0b69ae1b1da
jacobcalv <jacobcalvino@outlook.com>
2019-07-31 20:04:56 -0500

    added bucketlist generator and linked to homepage

--- js/bucketlistgenerator.js
+++ js/bucketlistgenerator.js
@@ -2,7 +2,7 @@ function randomItem(items){
     return items[Math.floor(Math.random()*items.length)];
 }
 
-const items = ['sky diving', 'Climb a Mountain', 'Run a Marathon', 'Travel to Every State', 'Learn How to Rock Climb', 'a', 'b', 'c', 'd', 'e', 'f', 'g'];
+const items = ['Sky Diving', 'Climb a Mountain', 'Run a Marathon', 'Travel to Every State', 'Learn How to Rock Climb', 'Go to Italy', 'Learn a New Language', 'Fly to Japan', 'Ride in Hot Air Balloon', 'Learn Karate', 'Learn How to Beat-Box', 'Do a Triathalon'];
 
 const selectedItem = randomItem(items);
 
